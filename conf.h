096a6858635cb2386ff900c435968f523046e441
Leo Zulfiu <leo.zulfiu@gmx.ch>
2016-10-10 15:09:20 +0200

    finished project

---
+++ conf.h
@@ -0,0 +1,18 @@
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+// CONFIG1
+#pragma config FOSC = INTOSCCLK // Oscillator Selection bits (INTRC oscillator; CLKO function on RA6/OSC2/CLKO pin and port I/O function on RA7/OSC1/CLKI pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is digital I/O, MCLR internally tied to VDD)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off)
+#pragma config CCPMX = RB3      // CCP1 Pin Selection bit (CCP1 function on RB0)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// CONFIG2
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal External Switchover mode disabled)
---
+++ main.c
@@ -0,0 +1,124 @@
+#include <xc.h>
+#include "conf.h"
+
+#define _XTAL_FREQ      4000000
+#define TMR2PRESCALE    16
+
+// TONES  ==========================================
+#define  c     261
+#define  d     293
+#define  e     329
+#define  f     349
+#define  g     392
+#define  a     440
+#define  b     493
+#define  C     523
+#define  D     587
+#define  E     659
+#define  F     698
+#define  G     784
+// Define a special note, 'R', to represent a rest
+#define  R     0
+// ==================================================
+
+int tempo = 2000;
+
+int current_tone = 0; //current playing note
+int current_tone_duration = 0; //current note duration
+
+void SetupClock() {
+    OSCCONbits.IRCF = 0b110; //set internal oscillator to 4 Mhz
+    OSCCONbits.SCS = 0b00; //use oscillator defined by FOSC
+}
+
+void PWM1_Init(long desired_frequency) {
+    PR2 = (_XTAL_FREQ / (desired_frequency * 4 * TMR2PRESCALE)) - 1;
+    current_tone = desired_frequency;
+}
+
+int PWM_Max_Duty() {
+    return (_XTAL_FREQ / (current_tone * TMR2PRESCALE));
+}
+
+void PWM1_Duty(unsigned int duty) {
+    if (duty < 1024) {
+        duty = ((float) duty / 1023) * PWM_Max_Duty();
+        CCP1X = duty & 2;
+        CCP1Y = duty & 1;
+        CCPR1L = duty >> 2;
+    }
+}
+
+void PWM1_Mode_On() {
+    CCP1M3 = 1;
+    CCP1M2 = 1;
+}
+
+void PWM1_Start() {
+    PWM1_Mode_On();
+
+#if TMR2PRESCALE == 1
+    T2CKPS0 = 0;
+    T2CKPS1 = 0;
+#elif TMR2PRESCALE == 4
+    T2CKPS0 = 1;
+    T2CKPS1 = 0;
+#elif TMR2PRESCALE == 16
+    T2CKPS0 = 1;
+    T2CKPS1 = 1;
+#endif
+    TMR2ON = 1;
+}
+
+void PWM1_Stop() {
+    CCP1M3 = 0;
+    CCP1M2 = 0;
+}
+
+void playTone() {
+    //TODO: The Tone R is not implemented yet.
+    if (current_tone > 0) {
+        PWM1_Init(current_tone);
+        PWM1_Duty(511);
+
+        //delay_ms cannot take a variable as parameter, only constant values...
+        //these values are actually hardcoded. The formula is: tempo/all used beats
+        if (current_tone_duration == 250) {
+            __delay_ms(250);
+        } else if (current_tone_duration == 500) {
+            __delay_ms(500);
+        } else if (current_tone_duration == 1000) {
+            __delay_ms(1000);
+        }
+        PWM1_Duty(0);
+    }
+}
+
+int main(int argc, char** argv) {
+    short melody[] = {g, g, a, g, C, b, g, g, a, g, D, C, g, g, G, E, C,
+        b, a, F, F, E, C, D, C};
+    char beats[] = {8, 8, 4, 4, 4, 2, 8, 8, 4, 4, 4, 2, 8, 8, 4, 4, 4,
+        4, 4, 8, 8, 4, 4, 4, 2};
+
+    char MELODY_LENGTH = sizeof (melody) / sizeof (melody[0]);
+
+    SetupClock();
+    TRISBbits.TRISB3 = 0; //set Pin B3 as output
+    PORTB = 0x00;
+
+    PWM1_Init(500); //exact value doesn't matter at all, just not 0
+    PWM1_Duty(0);
+    PWM1_Start();
+
+    while (1) {
+        for (int i = 0; i < MELODY_LENGTH; i++) {
+            current_tone = melody[i];
+            current_tone_duration = tempo / beats[i];
+
+            playTone();
+            __delay_ms(10);
+        }
+        __delay_ms(2000);
+    }
+    return 0;
+}
